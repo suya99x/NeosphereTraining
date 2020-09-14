ac94d1f55702bc420bb9fa03d0dadc121bc9e195
sudin <sudinshk@gmail.com>
2020-09-14 11:38:14 +0545

    folder searc

---
+++ folderSearch.py
@@ -0,0 +1,41 @@
+import os
+
+
+def search_word(file_content, folder_file_name, input_word_capitalized):
+    lines_counter = 1
+
+    for words in file_content:
+        words_capitalize = words[:-1].upper()
+        if words_capitalize == input_word_capitalized:
+            print("The word {} found in the file {}\\{} on line:".format(words_capitalize, os.path.basename(os.getcwd()), folder_file_name), lines_counter)
+        elif input_word_capitalized in words_capitalize:
+            print("The word is {} found in the file {}\\{} on line:".format(words_capitalize, os.path.basename(os.getcwd()), folder_file_name), lines_counter)
+        '''elif input_word_capitalized == countries_capitalize[:inputStringCapitalizeLength]:      
+            print("The word is {} found on line:".format(countries_capitalize),lines_counter)'''
+        lines_counter = lines_counter + 1
+
+
+def search_folders(input_capitalized,search_directory):
+    if not search_directory.upper() == "N":
+        directory_search=search_directory
+    else:
+        directory_search=os.getcwd()
+    folders = os.listdir(directory_search)
+    for folder in folders:
+        if os.path.isfile(folder) and folder.endswith(".txt"):
+            try:
+                open_file = open(folder, "r")
+                read_file = open_file.readlines()
+                search_word(read_file, folder, input_capitalized)
+            except Exception:
+                print("exception")
+        elif os.path.isdir(folder):
+            os.chdir(folder)
+            search_folders(input_capitalized,os.getcwd())
+            os.chdir("../")
+
+
+inputSearchWord = input("Enter word to search::")
+inputDirectory = input("Enter the directory to Search:")
+inputSearchWordCapital = inputSearchWord.upper()
+search_folders(inputSearchWordCapital, inputDirectory)
